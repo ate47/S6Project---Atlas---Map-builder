64e3233329601a2b53c2123300d463b3ea10b56e
ATE47 <wilate@yahoo.fr>
2020-02-27 13:20:49 +0100

    feat: add error message dialog

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,9 +1,18 @@
+import javax.swing.JOptionPane;
+
 import ssixprojet.builder.BuilderLoaderFrame;
 
 public class Start {
 
 	public static void main(String[] args) {
-		new BuilderLoaderFrame().setVisible(true);
+		try {
+			new BuilderLoaderFrame().setVisible(true);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.getClass().getName() + ": " + e.getMessage(), "An error occurred",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(-1);
+		}
 	}
 
 }
